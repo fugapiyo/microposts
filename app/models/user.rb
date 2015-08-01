c544b12fcc593fbe20572f1caa6b2d366904b0f7
fugapiyo <amwtpgaj@softbank.ne.jp>
2015-08-01 13:21:06 +0000

    add home view's timeline

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,9 @@ class User < ActiveRecord::Base
   def following?(other_user)
     following_users.include?(other_user)
   end
+
+  # タイムライン取得
+  def feed_items
+    Micropost.where(user_id: following_user_ids + [self.id])
+  end
 end
